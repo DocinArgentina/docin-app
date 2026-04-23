9ee35e1240be69aba79b3bee7fc9da3fbbffec32
DocinArgentina <docin.argentina@gmail.com>
2026-04-23 10:20:05 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -184,7 +184,7 @@ const Logo = ({ size = "md", dark = false }) => {
   const h = heights[size];
   return (
     <div className="flex items-center" style={{ height: h }}>
-     <img src="/logo_doc_in.png" alt="DOC In" style={{ height: h * 1.5, width: "auto", maxHeight: 54, filter: dark ? "brightness(0) invert(1)" : "none" }} />
+   <img src="/logo_doc_in.png" alt="DOC In" style={{ height: h * 3, width: "auto", maxHeight: 120, marginTop: -h, marginBottom: -h, filter: dark ? "brightness(0) invert(1)" : "none" }} />
     </div>
   );
 };
